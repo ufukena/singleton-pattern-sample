3923d6cf4c2f116f6050d4dacf393f6dc96c602a
Ufukena <ufukena@gmail.com>
2022-03-31 03:44:11 +0300

    Add project files.

---
+++ SingletonTestApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace SingletonTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Thread t1 = Thread.CurrentThread;
+            t1.Name = "Thread 1";
+
+            ////////////////////////////////////////////////////
+            
+            Thread t2 = new Thread( () => Upper());
+            Thread t3 = new Thread( () => Lower());
+
+            t2.Start();
+            t3.Start();
+
+            void Upper()
+            {
+                for (int i = 0; i < 10; i++)
+                {                    
+                    Console.WriteLine("Thread 2 : " + SingletonTest.GetInstance.Number++);
+                    Thread.Sleep(1000);
+                }
+            }
+
+            void Lower()
+            {
+                for (int i = 0; i < 10; i++)
+                {                    
+                    Console.WriteLine("Thread 3 : " + SingletonTest.GetInstance.Number++);
+                    Thread.Sleep(1300);
+                }
+            }
+
+            Console.WriteLine(t1.Name + " Begin..");
+            SingletonTest.GetInstance.Dispose();
+            Console.WriteLine("Disposed");
+
+        }
+    }
+}
---
+++ SingletonTestApp/SingletonTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace SingletonTestApp
+{
+    public sealed class SingletonTest : IDisposable
+    {
+        private bool _disposed;
+        private static volatile SingletonTest _instance;
+        private static readonly object _instanceLock = new object();
+       
+
+        private SingletonTest() {
+
+        }
+
+        public static SingletonTest GetInstance
+        {
+            get 
+            {               
+
+                if (_instance != null) return _instance;
+
+                //Check for multi thread..create instance only one time..
+                lock (_instanceLock)
+                {
+                    if (_instance == null) {
+                        _instance = new SingletonTest();
+                    }
+                        
+                }
+
+                return _instance;
+            }
+        }
+
+        public int Number { get; set; }
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                _instance = null;
+            }
+
+            _disposed = true;
+
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+
+
+
+
+
+
+
+    }
+}
